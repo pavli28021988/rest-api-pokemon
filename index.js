838ed2c6b09a5f5cb4d7db3b9b070d3fb7c0c2c2
Pavlina Ivanova <pavlina33331988@gmail.com>
2024-02-02 15:31:47 +0200

    added
    added

---
+++ index.js
@@ -0,0 +1,51 @@
+﻿
+
+// -----------------then()  and   catch()--------------
+
+/* fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+/* .then(response => console.log(response)) */
+/* .then(response=> {
+    if(!response.ok){
+        throw new Error('Could not fetch resource')
+    }
+    return response.json();
+})
+/* .then(data => console.log(data.game_indices[0].game_index)) */
+
+/* .then(data=> console.log(data))
+.catch(error => console.error(error));
+  */ 
+
+
+
+//--------------async await----------------------------------------//
+
+
+
+
+
+/* fetchData(); */
+async function fetchData(){
+    try{
+       
+        const pokemonName = document.getElementById('pokemonName').value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+        if(!response.ok){
+            throw new Error('Could not fetch resource!')
+        }
+
+        const data = await response.json();
+        //console.log(data.sprites.back_default);
+        const imgData = data.sprites.back_default
+        const imgElement = document.getElementById('pokemonSprite');
+        imgElement.src = imgData;
+        imgElement.style.display = 'block';
+
+        
+
+    }
+    catch{
+        console.error(error);
+    }
+}
